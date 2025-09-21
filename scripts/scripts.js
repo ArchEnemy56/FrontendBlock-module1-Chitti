ea94a3391d9b4a37e09d32f9ddd116f6c6bfec38
ArchEnemy56 <job.travel56@gmail.com>
2025-09-21 23:52:05 +0500

    les-19

---
+++ scripts/scripts.js
@@ -0,0 +1,49 @@
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 200) {
+        header.classList.add('scroll');
+    }
+    else {
+        header.classList.remove('scroll');
+    }
+});
+
+const links = document.querySelectorAll(".banner-btn");
+
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+window.onscroll = function () { scrollFunction() };
+
+const upbuttons = document.querySelectorAll(".button-up");
+
+for (const upbutton of upbuttons) {
+    upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById('btnUp').className = 'button-up visible';
+    } else {
+        document.getElementById('btnUp').className = 'button-up hidden';
+    }
+}
