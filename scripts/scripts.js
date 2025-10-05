648b2d7e38f84cd9b881749db4c7f5c95d6fb521
ArchEnemy56 <job.travel56@gmail.com>
2025-10-05 16:25:03 +0500

    les-24

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,10 @@
+// Smooth
+
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
     if (window.scrollY > 200) {
         header.classList.add('scroll');
-    }
-    else {
+    } else {
         header.classList.remove('scroll');
     }
 });
@@ -23,30 +24,7 @@ function clickHandler(e) {
     });
 }
 
-window.onscroll = function () { scrollFunction() };
-
-const upbuttons = document.querySelectorAll(".button-up");
-
-for (const upbutton of upbuttons) {
-    upbutton.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-    e.preventDefault();
-    const href = this.getAttribute("href");
-
-    document.querySelector(href).scrollIntoView({
-        behavior: "smooth"
-    });
-}
-
-function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        document.getElementById('btnUp').className = 'button-up visible';
-    } else {
-        document.getElementById('btnUp').className = 'button-up hidden';
-    }
-}
+// Burger Menu
 
 const btnBurger = document.getElementById("btnBurger");
 const modal = document.getElementById("modal");
@@ -59,3 +37,30 @@ btnBurger.addEventListener("click", function () {
 btnClose.addEventListener("click", function () {
     modal.classList.remove("active");
 });
+
+// Button Up
+
+window.onscroll = function () { scrollFunction() };
+
+const upbuttons = document.getElementById("btnUp");
+
+for (const upbutton of upbuttons) {
+upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+behavior: "smooth"
+});
+}
+
+function scrollFunction() {
+if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+document.getElementById('btnUp').className = 'btn-up visible';
+} else {
+document.getElementById('btnUp').className = 'btn-up hidden';
+}
+}
